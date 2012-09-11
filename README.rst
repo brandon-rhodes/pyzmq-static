fe2d6073aa7c1f1249556f7f9fe63d6085c95ab8
Brandon Rhodes <brandon@rhodesmill.org>
2012-09-11 18:41:30 -0400

    Changed the README to encourage a move to "pyzmq"

--- README.rst
+++ README.rst
@@ -1,84 +1,44 @@
-This package combines the **zmq** Python package
-with a bundled copy of ØMQ
-so that you do not have to install ØMQ separately on your system.
-This version combines:
+**We bring you good news!**
 
-* ØMQ 2.1.11 — http://www.zeromq.org/
-* PyZMQ 2.1.11 — http://pypi.python.org/pypi/pyzmq/
+The official maintainer of the Python
+`pyzmq <http://pypi.python.org/pypi/pyzmq/>`_ package,
+`@minrk <https://github.com/minrk>`_,
+has `merged our work <https://github.com/zeromq/pyzmq/pull/205>`_
+into the main project!
 
-On Linux and MacOS systems (but not on Windows or FreeBSD),
-the ``libuuid`` library code is also compiled in:
+That's right — the official version of PyZMQ
+can now detect the absence of a system-wide ØMQ library
+and respond by building and linking to its own copy of the library.
+The PyZMQ distribution carries its own copy of the source code
+so that no network access is necessary
+for its install to complete successfully.
 
-* util-linux-ng 2.19 — http://mirror.anl.gov/pub/linux/utils/util-linux-ng/
+This means your project can now simply depend upon normal ``pyzmq``
+instead of the separate name ``pyzmq-static``.
 
-PyZMQ is the official interface between Python
-and the ØMQ messaging library.
-The official distribution is called **pyzmq** on PyPI,
-and is maintained by Brian E. Granger.
+The main PyZMQ gained this superpower with its 2.2.0.1 release.
+If you need to use an older version of PyZMQ,
+then you can simply hard-code a dependency
+in your ``requirements.txt`` or ``setup.py``
+against whatever older version of ``pyzmq-static``
+you would like to keep using::
 
-This **pyzmq-static** distribution was created by Brandon Rhodes
-after he became frustrated with having to install both libuuid-dev
-and ZeroMQ itself on every machine where he then wanted to install **pyzmq**.
-It is now maintained by Evan Borgstrom.
+    pyzmq-static==2.1.11.2
 
-Whether you use Linux or Windows,
-this package should download and compile with a quick **pip** **install**
-without requiring any dependencies beyond the normal tools
-for building Python extension modules at all
-(which are, specifically, the GNU C and C++ compilers for POSIX systems,
-and the free Microsoft Visual C++ 2008 Express for Windows machines).
+Thanks again to `Evan Borgstrom <https://github.com/borgstrom>`_
+for helping me (`Brandon Rhodes <https://github.com/brandon_rhodes>`_)
+maintain this project,
+and thanks to our users for letting us know
+how often this distribution of ``pyzmq`` helped you get off the ground!
 
 Changelog
 ---------
 
-| 2012-04-28 - 2.1.11 - ØMQ and PyZMQ 2.1.11
+| 2012-08-20 — Project end — `@minrk`_ merges our work!
+| 2012-04-28 — 2.1.11 — ØMQ and PyZMQ 2.1.11
 | 2011-06-15 — 2.1.7.1 — Fixed compilation under Windows.
 | 2011-05-22 — 2.1.7 — ØMQ and PyZMQ 2.1.7.
 | 2011-04-02 — 2.1.4 — ØMQ 2.1.4; PyZMQ 2.1.1; util-linux-ng 2.19.
 | 2010-11-17 — 2.0.10 — ØMQ 2.0.10; FreeBSD support.
 | 2010-09-27 — 2.0.8 — Mac OS X support.
 | 2010-09-15 — 2.0.7a — World debut!
-
-Copying
--------
-
-The "setup.py" and "get.sh" files that power this package
-are offered under the BSD license.
-They build a shared library
-that includes code from three different pieces of software.
-To use the resulting library,
-you must adhere to the licensing terms of all three projects,
-which are as follows:
-
-* The UUID routines from "util-linux-ng": BSD
-* The "zeromq" source: LGPL
-* The "pyzmq" source: LGPL
-
-So, okay, those are not very restrictive licensing terms.
-But still.
-See the source files themselves for more information.
-And thanks to Jeff Garbers for helping us get the package
-working on MacOS X;
-to Tyler Tarabula for the FreeBSD support;
-and to Benjamin RK for helping me rewrite ``setup.py``
-when PyZMQ split from being one C extension to almost a dozen!
-
-Development
------------
-
-The source of **pyzmq-static** lives at GitHub:
-
-https://github.com/brandon-rhodes/pyzmq-static
-
-You can report bugs and problems here:
-
-https://github.com/brandon-rhodes/pyzmq-static/issues
-
-The development tree is accompanied by a small *get.sh* shell script
-that re-fetches all of the original source distributions
-for ØMQ, pyzmq, and libuuid, and rebuilds the *include* and *src*
-directories using the original files.
-When new versions of these dependencies come out,
-I update the *get.sh* script,
-tweak the result until it compiles cleanly under Linux and Windows,
-and release a new version of **pyzmq-static**.
--- setup.py
+++ setup.py
@@ -1,213 +1,23 @@
 # -*- coding: utf-8 -*-
-import distutils.util
-import os
-import sys
-from distutils.core import setup, Extension, Command
-from glob import glob
 
-from traceback import print_exc
-from unittest import TextTestRunner, TestLoader
-from os.path import splitext, basename, join as pjoin
-
-try:
-    import nose
-except ImportError:
-    nose = None
-
-#-----------------------------------------------------------------------------
-# Extra commands
-#-----------------------------------------------------------------------------
-
-class TestCommand(Command):
-    """Custom distutils command to run the test suite."""
-
-    user_options = [ ]
-
-    def initialize_options(self):
-        self._dir = os.getcwd()
-
-    def finalize_options(self):
-        pass
-    
-    def run_nose(self):
-        """Run the test suite with nose."""
-        return nose.core.TestProgram(argv=["", '-vvs', pjoin(self._dir, 'zmq', 'tests')])
-    
-    def run_unittest(self):
-        """Finds all the tests modules in zmq/tests/ and runs them."""
-        testfiles = [ ]
-        for t in glob(pjoin(self._dir, 'zmq', 'tests', '*.py')):
-            name = splitext(basename(t))[0]
-            if name.startswith('test_'):
-                testfiles.append('.'.join(
-                    ['zmq.tests', name])
-                )
-        tests = TestLoader().loadTestsFromNames(testfiles)
-        t = TextTestRunner(verbosity = 2)
-        t.run(tests)
-    
-    def run(self):
-        """Run the test suite, with nose, or unittest if nose is unavailable"""
-        # crude check for inplace build:
-        try:
-            import zmq
-        except ImportError:
-            print_exc()
-            print ("Could not import zmq!")
-            print ("You must build pyzmq with 'python setup.py build_ext --inplace' for 'python setup.py test' to work.")
-            print ("If you did build pyzmq in-place, then this is a real error.")
-            sys.exit(1)
-        
-        if nose is None:
-            print ("nose unavailable, falling back on unittest. Skipped tests will appear as ERRORs.")
-            return self.run_unittest()
-        else:
-            return self.run_nose()
-
-cmdclass = {'test':TestCommand}
-
-#-----------------------------------------------------------------------------
-# Static linking extras
-#-----------------------------------------------------------------------------
-
-static_sources = glob('src/*')
-include_dirs = ['include']
-
-if hasattr(sys, 'getwindowsversion'):
-    include_dirs.append('include_nt')
-else:
-    if sys.platform == 'darwin':
-        include_dirs.append('include_macosx')
-        include_dirs.append('include_uuid')
-        static_sources.extend(glob('src_uuid/*.c'))
-    elif sys.platform.startswith('freebsd'):  # for example, 'freebsd7'
-        include_dirs.append('include_freebsd')
-    else:
-        include_dirs.append('include_linux')
-        include_dirs.append('include_uuid')
-        static_sources.extend(glob('src_uuid/*.c'))
-
-#-----------------------------------------------------------------------------
-# Extensions
-#-----------------------------------------------------------------------------
-
-include_dirs.append(pjoin('zmq', 'utils'))
-
-def pxd(subdir, name):
-    return os.path.abspath(pjoin('zmq', subdir, name+'.pxd'))
-
-czmq = pxd('core', 'czmq')
-allocate = pxd('utils', 'allocate')
-buffers = pxd('utils', 'buffers')
-
-submodules = dict(
-    core = {'constants': [czmq],
-            'error':[czmq],
-            'poll':[czmq, allocate], 
-            'stopwatch':[czmq],
-            'context':[pxd('core', 'socket'), czmq],
-            'message':[czmq, buffers],
-            'socket':[pxd('core', 'context'), pxd('core', 'message'), 
-                      czmq, allocate, buffers],
-            'device':[czmq],
-            'version':[czmq],
-    },
-    devices = {
-            'monitoredqueue':[buffers, czmq],
-    },
-    utils = {
-            'initthreads':[czmq]
-    }
-)
-
-extensions = [Extension('zmq.libzmq',
-                        sources=static_sources,
-                        include_dirs=include_dirs)]
-
-for submod, packages in submodules.items():
-    for pkg in sorted(packages):
-        sources = [pjoin('zmq', submod, pkg+'.c')]
-        ext = Extension(
-            'zmq.%s.%s'%(submod, pkg),
-            sources = sources,
-            include_dirs = include_dirs,
-        )
-        extensions.append(ext)
-
-package_data = {'zmq':['*.pxd'],
-                'zmq.core':['*.pxd'],
-                'zmq.devices':['*.pxd'],
-                'zmq.utils':['*.pxd', '*.h'],
-}
-
-# Visual Studio needs some encouragement to actually compile everything.
-
-if hasattr(sys, 'getwindowsversion'):
-
-    libzmq = extensions[0]
-    other_extensions = extensions[1:]
-
-    # When compiling the C++ code inside of libzmq itself, we want to
-    # avoid "warning C4530: C++ exception handler used, but unwind
-    # semantics are not enabled. Specify /EHsc".
-
-    libzmq.extra_compile_args.append('/EHsc')
-
-    # Because Visual Studio is given the option "/EXPORT:initlibzmq"
-    # when compiling libzmq, so we need to provide such a function.
-
-    libzmq.sources.append(r'src_nt\initlibzmq.c')
-
-    # And things like sockets come from libraries that must be named.
-
-    libzmq.libraries.append('rpcrt4')
-    libzmq.libraries.append('ws2_32')
-
-    # Then all of the real extensions need to link against that first
-    # library.
-
-    plat = distutils.util.get_platform()
-    temp = 'temp.%s-%s' % (plat, sys.version[0:3])
-    for other in other_extensions:
-        thisdir = os.path.dirname(__file__)
-        libdir = os.path.join(thisdir, 'build', temp, 'Release', 'src')
-        other.library_dirs.append(libdir)
-        other.libraries.append('libzmq')
-
-# The setup() call itself.
+from distutils.core import setup
 
 long_description = open('README.rst').read().decode('utf-8')
-if sys.version_info < (2, 6):  # work around Python 2.5 UnicodeEncodeError
-    description = u'zmq package that compiles its own 0MQ / ZeroMQ'
-    long_description = long_description.encode('utf-8')
-else:
-    description = u'zmq package that compiles its own ØMQ / 0MQ / ZeroMQ'
+description = u'Obsolete fork of pyzmq'
 
 setup(name='pyzmq-static',
-      version='2.1.11.2',
+      version='2.2',
       description=description,
       long_description=long_description,
       author='Brandon Craig Rhodes',
       author_email='brandon@rhodesmill.org',
       maintainer='Evan Borgstrom',
       maintainer_email='evan@fatbox.ca',
-      url='https://github.com/brandon-rhodes/pyzmq-static',
-      cmdclass = cmdclass,
-      packages = ['zmq', 'zmq.tests', 'zmq.eventloop', 'zmq.log', 'zmq.core',
-                  'zmq.devices', 'zmq.utils'],
-      package_data = package_data,
-      ext_modules= extensions,
-      classifiers = [
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Financial and Insurance Industry',
-        'Intended Audience :: Science/Research',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: BSD License',
-        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
-        'Operating System :: MacOS :: MacOS X',  # passes tests
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX',
-        'Topic :: System :: Networking'
-        ]
+      url='https://github.com/zeromq/pyzmq',
+
+      # Should we release a 2.2 tarball consisting only of the README
+      # and the following requirement, so that users get automatically
+      # migrated over if they did not pin their pyzmq-static version?
+      #
+      # install_requires = ['pyzmq'],
       )
